1e2fbf6555b7444c16ce405a5e045a9fefabc60e
Janeworld <bellomoromoke2019@gmail.com>
2025-12-24 16:05:51 +0200

    src folder added

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+        <section className='w-full bg-blue-500 h-[500px] flex flex-col justify-center items-center gap-8  '>
+            <img src="" alt="" />
+            <h2>Let TeachApt tutors unlock your potential</h2>
+        </section>
+    </div>
+  )
+}
