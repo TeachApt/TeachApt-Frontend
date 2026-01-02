c6cc2c201cfaf74d6313d8e332229cdeaa0b3ecd
Janeworld <bellomoromoke2019@gmail.com>
2026-01-02 19:29:41 +0200

    styling added for hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div>
-        <section className='w-full bg-blue-200  h-[500px] relative overflow-hidden  '>
+        <section className='w-full p-10 bg-blue-200 relative overflow-hidden  '>
             <div className='mx-auto px-6 py-20 grid grid-cols-1 md:grid-cols-2 gap-12 items-center text-black'>
 
-              <div>
-                  <h2 className='text-3xl md:text-4xl font-bold leading-tight'> Let <span className="text-blue-600">TeachAPT</span> Tutors Unlock
+              <div className='flex flex-col items-center justify-center md:items-start md:justify-start'>
+                  <h2 className='text-4xl md:text-5xl font-bold leading-tight'> Let <span className="text-blue-600">TeachAPT</span> Tutors Unlock
             <br /> Your Child’s Potential!</h2>
 
             <p className='mt-6 max-w-lg'>
@@ -28,22 +28,31 @@ export default function Hero() {
          <div className='relative flex justify-center'>
            <Image alt='student learning'
            src={"/student.png"}
-           width={400}
-           height={400}
-           className='relative z-10' />
+           width={500}
+           height={450}
+           className='relative z-10 -mt-20' />
          
+         {/* floating badge */}
+         <div className="absolute flex gap-6 items-center top-10 right-0 bg-gray-400 shadow-lg px-8 py-2 rounded-sm text-white text-sm">
+          <img src="/vector.png" alt="badge-icon" className="w-6 h-6"/>
+          <div  className="flex flex-col text-xs items-center text-center">
+            <p className='font-bold'>500+</p>
+            <p >Expert <br/> Instructor</p>
+          </div>
+
+         </div>
 
          {/* decorative circle */}
          <div className='absolute '>
                   <Image alt='decorative circle'
            src={"/hero.png"}
-           width={600}
-           height={500}
+           width={650}
+           height={550}
            className='mt-30'
             /> 
          </div>
 
-         <div className="w-[545px] mx-auto bg-white shadow-md px-8 py-1 mt-75 absolute z-20 justify-center items-center">
+         <div className="w-[585px] mx-auto bg-white shadow-md px-8 py-1 mt-80 absolute z-20 justify-center items-center">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 justify-center text-center">
           <div className='text-left'>
             <p className="font-bold text-sm">100+</p>
