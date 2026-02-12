6c760d4e8a53e9e969eef470480674d3df7def06
Olutoke Adedamola <olutokeadedamola@gmail.com>
2026-02-12 16:38:18 +0100

    fix(lint-setup): files formatting
    - Refactor and format code across multiple components for improved readability and consistency.
    - Adjusted JSON configuration for better structure and updated layout components for cleaner rendering.
    - Enhanced the Hero and Educator sections with refined styling and layout adjustments.

--- src/components/Partner/Partners.tsx
+++ src/components/Partner/Partners.tsx
@@ -12,9 +12,7 @@ export default function Partners() {
   return (
     <section className="py-16 bg-white">
       <div className="max-w-7xl mx-auto px-6 text-center">
-        <h2 className="text-2xl font-bold text-blue-600 mb-12">
-          Our Partners
-        </h2>
+        <h2 className="text-2xl font-bold text-blue-600 mb-12">Our Partners</h2>
 
         <div className="flex flex-wrap justify-center items-center gap-20 opacity-60">
           {partners.map((partner) => (
--- src/components/Services/FeaturedCard.tsx
+++ src/components/Services/FeaturedCard.tsx
@@ -1,10 +1,9 @@
-
 type FeaturedCardProps = {
-    icon: string;
-    title:string;
-    description:string;
-}
-export default function FeaturedCard({icon, title, description}: FeaturedCardProps) {
+  icon: string;
+  title: string;
+  description: string;
+};
+export default function FeaturedCard({ icon, title, description }: FeaturedCardProps) {
   return (
     <div className="bg-white border border-gray-200 rounded-r-xl p-4 shadow-sm hover:shadow-lg transition">
       <img src={icon} alt={title} className="w-8 h-8 mb-4 bg-blue-500 p-2 rounded-sm " />
@@ -12,5 +11,5 @@ export default function FeaturedCard({icon, title, description}: FeaturedCardPro
 
       <p className="text-sm max-w-sm text-gray-600 leading-relaxed">{description}</p>
     </div>
-  )
+  );
 }
